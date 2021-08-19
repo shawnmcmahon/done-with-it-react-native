b5cf7288cd2fea70d5f6ebd8e80019c9de61edb8
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-08-18 19:30:43 -0600

    Add 3 containers to practice react flexbox

--- App.js
+++ App.js
@@ -1,41 +1,33 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Image,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
-  TouchableHighlight,
-  Button,
-  Alert,
-  Platform,
-  StatusBar,
-} from 'react-native';
+import { View } from 'react-native';
 
-// View -> UIView
 export default function App() {
   return (
-    <SafeAreaView style={[styles.container, containerStyle]}>
-      <Button
-        color="orange"
-        title="Click Me"
-        onPress={() =>
-          Alert.prompt('My title', 'My Message', (text) => console.log(text))
-        }
+    <View
+      style={{
+        backgroundColor: '#fff',
+        flex: 1,
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: 'dodgerblue',
+          flex: 1,
+        }}
       />
-    </SafeAreaView>
+      <View
+        style={{
+          backgroundColor: 'gold',
+          flex: 1,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: 'tomato',
+          flex: 1,
+        }}
+      />
+    </View>
   );
 }
-
-const containerStyle = { backgroundColor: 'orange' };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'orange',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-  },
-});
