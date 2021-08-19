ba69699506876ccd9b895b11c82b6780d6003f7e
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-08-18 18:45:08 -0600

    Add button to home screen

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  Button,
+} from 'react-native';
 
 // View -> UIView
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native</Text>
-      <Image
-        blurRadius={10}
-        fadeDuration={1000}
-        source={{
-          width: 200,
-          height: 300,
-          uri: 'https://picsum.photos/200/300',
-        }}
+      <Button
+        color="orange"
+        title="Click Me"
+        onPress={() => console.log('button tapped')}
       />
-      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
